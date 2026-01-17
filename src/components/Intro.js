4d1dd528776f0fef8d520d670be71385c7ff411f
swarcgit <matarr930@gmail.com>
2026-01-18 01:55:39 +0900

    skill

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -10,35 +10,35 @@ const Intro = () => {
 
   return (
     <section id="intro">
-      <div class="intro__inner">
-        <h1 class="intro__title">{introText.title}</h1>
-        <div class="intro__lines" aria-hidden="true">
-          <span class="line"></span>
-          <span class="line"></span>
-          <span class="line"></span>
-          <span class="line"></span>
-          <span class="line"></span>
-          <span class="line"></span>
-          <span class="line"></span>
+      <div className="intro__inner">
+        <h1 className="intro__title">{introText.title}</h1>
+        <div className="intro__lines" aria-hidden="true">
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
         </div>
-        <div class="intro__text">
-          <div class="text">
+        <div className="intro__text">
+          <div className="text">
             <div>{introText.desc[0]}</div>
             <div>{introText.desc[1]}</div>
             <div>{introText.desc[2]}</div>
           </div>
-          <div class="img">
+          <div className="img">
             <img src={about} alt="어바웃" />
           </div>
         </div>
-        <div class="intro__lines bottom" aria-hidden="true">
-          <span class="line"></span>
-          <span class="line"></span>
-          <span class="line"></span>
-          <span class="line"></span>
-          <span class="line"></span>
-          <span class="line"></span>
-          <span class="line"></span>
+        <div className="intro__lines bottom" aria-hidden="true">
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
         </div>
       </div>
     </section>
